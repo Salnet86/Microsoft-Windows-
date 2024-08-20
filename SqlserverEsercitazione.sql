8f20265e0af938dd415d0dd0cb6e56b784447beb
Tecnologie <dosal86@libero.it>
2024-08-20 10:46:01 +0200

    Add files via upload

---
+++ SqlserverEsercitazione.sql
@@ -0,0 +1,25 @@
+
+
+
+  
+  /*
+   SELECT deptno FROM dept
+   except
+   SELECT deptno FROM emp;*/
+
+   /*
+   SELECT deptno FROM dept
+   union
+   SELECT deptno FROM emp;*/
+
+   /*
+    SELECT deptno FROM dept
+    Intersect
+    SELECT deptno FROM emp;*/
+
+
+   select * from emp;
+  
+  
+  
+  --exec sp_helpindex employees
